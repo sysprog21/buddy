02916189fba6b514a4114bfd1ff4e2dae29b8b2b
Jim Huang <jserv.tw@gmail.com>
2019-05-01 17:41:19 +0800

    Format tweak for test messages

--- test-malloc.c
+++ test-malloc.c
@@ -1,47 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MSG(foo) fprintf(stderr, "Line %d: %s", __LINE__, foo)
+#define MSG(foo) fprintf(stderr, "Line %d: %s\n", __LINE__, foo)
 
 int main(int argc, char *argv[])
 {
     char *p, *q;
     double *d;
 
-    MSG("-- Test malloc() for unusual situations\n");
+    MSG("-- Test malloc() for unusual situations");
 
-    MSG("Allocate small block of 17 bytes \n");
+    MSG("Allocate small block of 17 bytes ");
     p = malloc(17);
     if (p == NULL)
-        MSG("* ERROR: Can't allocate even 17 bytes. Must be bug in malloc\n");
-    MSG("Write on allocated block\n");
+        MSG("* ERROR: Can't allocate even 17 bytes.");
+    MSG("Write on allocated block");
     p[0] = p[16] = 17;
 
-    MSG("Allocate big block of 4711 bytes\n");
+    MSG("Allocate big block of 4711 bytes");
     q = malloc(4711);
     if (q == NULL)
-        MSG("* ERROR: Could not allocate big block\n");
-    MSG("Write on allocated block\n");
+        MSG("* ERROR: Could not allocate big block");
+    MSG("Write on allocated block");
     q[4710] = 47;
-    MSG("Free big block\n");
+    MSG("Free big block");
     free(q);
 
-    MSG("Free small block\n");
+    MSG("Free small block");
     free(p);
 
-    MSG("Free NULL\n");
+    MSG("Free NULL");
     free(NULL);
 
-    MSG("Allocate zero\n");
+    MSG("Allocate zero");
     if ((p = malloc(0)) != NULL)
-        MSG("* ERROR: malloc(0) returned non NULL pointer!\n");
+        MSG("* ERROR: malloc(0) returned non NULL pointer!");
 
-    MSG("Free pointer from malloc(0)\n");
+    MSG("Free pointer from malloc(0)");
     free(p);
 
-    MSG("Test alignment for double\n");
+    MSG("Test alignment for double");
     if ((d = malloc(2 * sizeof(double))) == NULL)
-        MSG("* ERROR: malloc(2 * sizeof(double)) returned NULL\n");
+        MSG("* ERROR: malloc(2 * sizeof(double)) returned NULL");
     d[0] = d[1] = (double) 4711.4711;
     free(d);
 
